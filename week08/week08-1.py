65a48d0874a7becd7ef0c68cbc5eb4f39106a67e
zqiqii77 <abc0988313148@gmail.com>
2023-10-31 12:18:04 +0800

    week08

---
+++ week08/week08-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n<=0: 
+            return False
+        while n>1:
+            if n % 2 !=0:#餘數不是0,就失敗了
+                return False
+            else:
+                n=n//2
+
+
+        return True
---
+++ week08/week08-5.py
@@ -0,0 +1,8 @@
+b=a
+
+ans=0
+while a>0:
+	ans=ans*10+a%10
+	x  a=a//10
+
+print(f'{b}+{ans}={b+ans}')
